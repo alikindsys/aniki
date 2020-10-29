55fc0a4abdd77c8266f65ffad8e696f3ee276299
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-10-29 16:32:09 -0300

    Renames queue option to input

--- src/main/kotlin/base/core/cli/Parser.kt
+++ src/main/kotlin/base/core/cli/Parser.kt
@@ -3,4 +3,4 @@ package base.core.cli
 import kotlinx.cli.*
 
 val Parser = ArgParser("aniki")
-val queueFile by Parser.option(ArgType.String, "queue" , "q")
+val input by Parser.option(ArgType.String, "input" , "i").multiple()
