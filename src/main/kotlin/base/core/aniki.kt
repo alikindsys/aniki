a1ae86ccc0d3715c603887a11b5173001f38dc7e
Paulo Henrique Prates <t3ctotalmenterandom1@outlook.com>
2020-10-29 16:26:20 -0300

    Adds tryDownloadSeries function
    Base function that will attempt to download a series from a given uri on all available sources.

--- src/main/kotlin/base/core/aniki.kt
+++ src/main/kotlin/base/core/aniki.kt
@@ -1,6 +1,8 @@
 package base.core
 
 import com.google.gson.Gson
+import org.asynchttpclient.uri.Uri
+import org.openqa.selenium.WebDriver
 import java.io.File
 
 class aniki {
@@ -19,6 +21,21 @@ class aniki {
         fun saveCfg() {
             val file = File("./config.json")
             file.writeText(Gson().toJson(config))
+        }
+
+        fun tryDownloadSeries(uri: Uri, driver: WebDriver) : Boolean {
+            val provider = ProviderRegistry.firstOrNull{ it.ProviderUrl.authority == uri.authority } ?: return false
+            val anime = provider.getAnime(uri, driver)
+            println("[${provider::class.simpleName}] Detected anime : ${anime.Name}")
+            println("[${provider::class.simpleName}] Getting episodes. Please wait.")
+            val episodes = anime.getEpisodes(anime,driver)
+            println("[${provider::class.simpleName}] Got ${episodes.size} episodes.")
+            println("[${provider::class.simpleName}] Download of the series will begin shortly.")
+            for((i,episode) in episodes.withIndex()) {
+                val file = File(config.folder,"${anime.Name}/${(i+1)}.mp4")
+                episode.download(file)
+            }
+            return true
         }
     }
 }
